198998ce71a23aac06f12ca8e70f3738f4743fe6
Will Clifford <wtc8754@rit.edu>
2020-01-16 14:53:48 -0500

    Combining Parts 1 and 2

--- CTOverdoseCode.r
+++ CTOverdoseCode.r
@@ -1,11 +1,17 @@
 # Will Clifford
-# ECON403 Econometrics I - Paper I
+# ECON403 Econometrics I
+
+library(car)
+library(lmtest)
 
 # Overdose data from Data.gov and the State of Maryland.
 # ( See https://data.ct.gov/Health-and-Human-Services/Accidental-Drug-Related-Deaths-2012-2018/rybz-nyjw )
-ods_csv <- read.csv("~/econ/paper1/ODDeaths.csv")
+ods_csv <- read.csv("~/GitHub/CT-Overdose-Analysis/data/ODDeaths.csv")
 
 
+#####################################################################
+#                               PART 1                              #          
+#####################################################################
 ###      TOTALS IN DATA      ###
 #    Gender:
 table(ods_csv$Female) # more than 75% of overdose deaths are male
@@ -53,3 +59,30 @@ summary(ods_lm1 )
 summary(ods_lm1a)
 summary(ods_lm2 )
 summary(ods_lm2a)
+
+
+#####################################################################
+#                               PART 2                              #          
+#####################################################################
+#   1. Test of Joint Significance
+jnt_sig <- linearHypothesis(ods_lm1, c("FentanylAnalogue=0", "Oxymorphone=0", "OpiateNOS=0"))
+jnt_sig
+
+#   2. Quadratic Model
+ods_quad <- lm(NumDrugs~Age+I(Age^2)+Female+Black+Hispanic+Unknown.Other, data=ods_csv)
+summary(ods_quad)
+
+tp <- coefficients(ods_quad)["Age"]/(-2*coefficients(ods_quad)["I(Age^2)"])
+tp
+perc_tp <- ifelse(tp > 0, sum(ods_csv$Age > tp) / length(ods_csv$Age), "Negative Turning Point")
+perc_tp
+
+##  3. Group Drugs - from part 1
+summary(ods_lm1)
+
+#   4. Test for Homoskedasticity, Interpret, and Correct (if necessary)
+bptest(ods_lm1)
+coeftest(ods_lm1, hccm(ods_lm1))
+
+bptest(ods_quad)
+coeftest(ods_quad, hccm(ods_quad))
